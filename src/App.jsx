9390abb895c52b00efa06fbe1d46046af91662d3
OsamaAhmed201 <oa3885430@gmail.com>
2025-12-05 18:33:09 +0200

    edit URL Site

--- src/App.jsx
+++ src/App.jsx
@@ -49,10 +49,10 @@ export default function App() {
       { path: "Productbrand/:id", element: <ProtectedRouting><ProductBrand /></ProtectedRouting> },
       { path: "detalisProduct/:id", element: <ProtectedRouting><DetalisProduct /></ProtectedRouting> },
       { path: "shippingdetails/:id", element: <ProtectedRouting><ShippingDetails /></ProtectedRouting> },
-      { path: "allorders", element: <ProtectedRouting><AllorderUser /> </ProtectedRouting> },
+      { path: "/allorders", element: <ProtectedRouting><AllorderUser /> </ProtectedRouting> },
       { path: "/changePassword", element: <ProtectedRouting><ChangePassword /></ProtectedRouting> },
       { path: "/favsProduct", element: <ProtectedRouting><FavsProduct /></ProtectedRouting> },
-      { path: "/rooms", element: <ProtectedRouting><Rooms /></ProtectedRouting> },
+ 
    
 
     ]
